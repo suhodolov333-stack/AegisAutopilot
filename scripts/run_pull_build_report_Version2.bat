c2d5bfb70ae63cc2cd5b700adbd565be71b7cf32
suhodolov333-stack <suhodolov333@gmail.com>
2025-09-26 18:57:13 -0700

    Resolved Autobuild conflicts presrving local logic

--- scripts/run_pull_build_report_Version2.bat
+++ scripts/run_pull_build_report_Version2.bat
@@ -1,5 +1,11 @@
 @echo off
 set SCRIPT_DIR=%~dp0
+<<<<<<< Updated upstream
 set CONFIG=%SCRIPT_DIR%..\config\build-config_Version2.json
 
 powershell -ExecutionPolicy Bypass -File "%SCRIPT_DIR%pull_build_report_Version2.ps1" -ConfigPath "%CONFIG%"
+=======
+set CONFIG=%SCRIPT_DIR%..\config\build-config.json
+
+powershell -ExecutionPolicy Bypass -File "%SCRIPT_DIR%pull_build_report.ps1" -ConfigPath "%CONFIG%"
+>>>>>>> Stashed changes
